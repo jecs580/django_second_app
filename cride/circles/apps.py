2f934f1ab89e70d98d76d3e4806319b1fdb01535
Jorge Callisaya <sanchezenrique580@gmail.com>
2019-12-24 15:40:08 -0400

    Creacion de la Circles y agregando el modelo circles

---
+++ cride/circles/apps.py
@@ -0,0 +1,12 @@
+""""Circles app."""
+# Este archivo sirve para declarar a django nuestras aplicaciones, las variables que deben estar si o si son: "name" y "verbose name" pero tu le puedes agregar mas cosas si quieres.
+
+# Django
+from django.apps import AppConfig
+
+# El nombre de la clase sirve para instalarlo en INSTALLED_APPS en los settings
+class CirclesAppConfig(AppConfig):
+    """"Configuracion de la app de usuarios."""
+    name= 'cride.circles'
+    verbose_name='Circles' # El nombre "bonito" :V
+
---
+++ cride/circles/models/circles.py
@@ -0,0 +1,45 @@
+"""Modelo de Circulos"""
+
+#Django
+from django.db import models
+
+#Utilitis
+from cride.utils.models import CRideModel
+
+class Circle(CRideModel):
+    """Modelo de Circulos
+    
+    Un círculo es un grupo privado donde los viajes son ofrecidos y llevados por sus miembros. Para unirse a
+    un círculo, un usuario debe recibir un código de invasión único de un miembro del círculo existente
+    """
+    name = models.CharField('Nombre del Circulo',max_length=140)
+    slug_name=models.SlugField(unique=True,) # Slug es como un identificador para modelos que no sean de personas. En el modelo de personas o User el slug es el username
+    about= models.CharField('Descripcion del Circulo', max_length=255)
+    picture=models.ImageField(upload_to='circles/pictures',blank=True, null=True)
+
+    #Estidisticas
+    rides_offered=models.PositiveIntegerField(default=0)
+    rides_taken=models.PositiveIntegerField(default=0)
+    
+    is_verified=models.BooleanField(
+        'Verificacion de Circulo', # Esto permite sabes si el ciculo es oficial
+        default=False,
+        help_text='Los círculos verificados también se conocen como comunidades oficiales.')
+    is_public=models.BooleanField(default=True,
+        help_text= 'Los círculos públicos se enumeran en la página principal para que todos sepan sobre su existencia.'    
+    )
+    is_limited=models.BooleanField(
+        'Limitado',
+        default=False,
+        help_text='Los círculos limitados pueden crecer hasta un número fijo de miembros.')
+    members_limit=models.PositiveIntegerField(default=0,
+        help_text='Si el círculo es limitado, este será el límite en el número de miembros.'
+        )
+
+    def __str__(self):
+        """Retorna el nombre de Circulo"""
+        return  self.name
+
+    class Meta(CRideModel.Meta):
+        """"Clase Meta"""
+        ordering=['-rides_taken','-rides_offered']
