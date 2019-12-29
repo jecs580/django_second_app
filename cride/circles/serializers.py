a494f9885d50a3a2a043be119190abd0928f24e5
Jorge Callisaya <sanchezenrique580@gmail.com>
2019-12-28 20:18:31 -0400

    Creacion de la clase CircleSerializer y CreateCircleSerializer para mostrar lista de circulos y creacion de un nuevo circulo

--- cride/circles/serializers.py
+++ cride/circles/serializers.py
@@ -4,8 +4,17 @@ from rest_framework import serializers
 
 class CircleSerializer(serializers.Serializer):
      """Serializador de Circulos"""
+     # Para mostrar una lista de circulos en formato Json o XML
      name = serializers.CharField()
      slug_name=serializers.SlugField()
      rides_taken=serializers.IntegerField()
      rides_offered=serializers.IntegerField()
      members_limit=serializers.IntegerField()
+
+class CreateCircleSerializer(serializers.Serializer):
+     """Creacion de serializador de circulos"""
+     name=serializers.CharField(max_length=140)
+     slug_name=serializers.SlugField(max_length=40)
+     about=serializers.CharField(max_length=255,
+          required=False # por defecto el required es True.
+     ) # Esto tambien podria ser un textField por que puede tener una gran cantidad de caracteres, pero DRF no posee el campo, pero para complementar esto DRF no tiene un limite de caracteres.
--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,42 +1,33 @@
 """Vistas de Circulos"""
 
-#Django
+# Django
 # from django.http import JsonResponse  ; Respuesta que devuevle datos en formato Json
 
 # Django REST Framework
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-#Models
+# Models
 from cride.circles.models import Circle
 
+# Serializers
+from cride.circles.serializers import (CircleSerializer,CreateCircleSerializer)
 @api_view(['GET'])
 def list_circles(request):
     """Lista los Circulos"""
     circles=Circle.objects.filter(is_public=True)
-    data=[]
-    for circle in circles:
-        data.append({
-            'name':circle.name,
-            'slug_name':circle.slug_name,
-            'rides_taken':circle.rides_taken,
-            'rides_offered':circle.rides_offered,
-            'members_limit':circle.members_limit,
-        })
-    return Response(data)
+    # data=[]
+    # for circle in circles:
+    #     serializer=CircleSerializer(circle)
+    #     data.append(serializer.data)
+    serializer=CircleSerializer(circles,many=True)
+    return Response(serializer.data)
     # return JsonResponse(data,safe=False); Retorna los datos en Json, el atributo safe =True indica que los datos que se mandaran es un diccionario, si colocas en False, aceptara otros tipos de datos, por defecto safe esta en True.
 @api_view(['POST'])
 def create_circle(request):
     """Creacion de circulos"""
-    name=request.data['name']
-    slug_name=request.data['slug_name']
-    about=request.data.get('about','') # Esta forma de traer los datos de request nos ayuda para asignar un valor nosostros en caso de que no contenga nada el valor. Por lo general se usa cuando colocamos campos opcionales.
-    circle = Circle.objects.create(name=name,slug_name=slug_name,about=about)
-    data={
-        'name':circle.name,
-        'slug_name':circle.slug_name,
-        'rides_taken':circle.rides_taken,
-        'rides_offered':circle.rides_offered,
-        'members_limit':circle.members_limit,
-    }
-    return Response(data)
+    serializer=CreateCircleSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True) # Para validar los campos, por defecto no se tiene atributos, si colocas el atributo raise_exception=True, devolvera una respuesta de 400 al cliente, tambien devolvera un json con los errores que ocurrieron.
+    data=serializer.data
+    circle=Circle.objects.create(**data) # Desempaquetara todos las llave y valores que trae data.
+    return Response(CircleSerializer(circle).data) # Response se encargara de convertirlo a Json.
