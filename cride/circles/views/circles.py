6dbe344b56d489689e54b152898a1e9a7227d1f7
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-01-07 20:14:45 -0400

    Configurando que el perfil que cree el circle sea admin, agregando campos al serializer para is_limited y members_limit y validando que si o si los 2 campos esten o ninguno para crear el circulo limitado.

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 # Models
-from cride.circles.models import Circle # Esta llamada es de la forma de modulo, todos los archivo dentro del folder funcionan como uno. Y asi solo podemos llamarlo por su clase.
+from cride.circles.models import Circle,Membership # Esta llamada es de la forma de modulo, todos los archivo dentro del folder funcionan como uno. Y asi solo podemos llamarlo por su clase.
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
@@ -21,4 +21,17 @@ class CircleViewSet(viewsets.ModelViewSet): # El modelViewSet incluye las accion
         queryset=Circle.objects.all() # Traemos todos los datos de circles
         if self.action=='list':
             return queryset.filter(is_public=True) # Adicionamos un filtro mas para, mostrar solo los publicos
-        return queryset # Retorna todo el query si la accion no es list. Esto se usara para las demas acciones como update,retrieve,etc.  
+        return queryset # Retorna todo el query si la accion no es list. Esto se usara para las demas acciones como update,retrieve,etc.
+    
+    def perform_create(self,serializer):
+        """Asigna un admin de circulo al que crea el circulo"""
+        circle=serializer.save()
+        user=self.request.user # Usamos el user que viene de request una vez que se loguee.
+        profile=user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True, # Le colocamos que es administrador del circle
+            remaining_invitations=10 # Como admin le asignamos mas invitaciones.
+        )
